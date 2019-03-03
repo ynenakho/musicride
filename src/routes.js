a2dbeb1f69889f633ef5909fc8b7dd22af308676
terexion <sansyzbayev.alibek@gmail.com>
2019-03-02 23:35:35 -0800

    changed routing

--- src/routes.js
+++ src/routes.js
@@ -1,111 +1,104 @@
 import React from 'react';
-import { Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import ChooseSongs from './components/ChooseSongs/ChooseSongs'
 import PlayList from './components/PlayList/PlayList'
-import Header from './components/Header'
+import Car from './components/Car/Car'
 import App from './App'
 
 
 import './devstyles.css';
-import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 const NotFound = () => (<h1>Page was not found</h1>);
 
-const DevelopLinks = () => (
-    <div className="dev-links">
-        <ul className="dev-ul">
-            <li className="dev-li"><Link to="/">Main</Link></li>
-            <li className="dev-li"><Link to="/choose">Choose</Link> </li>
-            <li className="dev-li"><Link to="/play">Playlist</Link> </li>
-        </ul>
-    </div>
-)
-
-const AppliedRoute = ({path, Component, routeData, funcs}) => (
-    <Route path={path} render={(routeProps) => <Component {...routeProps} {...routeData} {...funcs}/>} />
+const AppliedRoute = ({ path, Component, routeData, funcs }) => (
+	<Route path={path} render={(routeProps) => <Component {...routeProps} {...routeData} {...funcs} />} />
 );
 
 
 
 class Routes extends React.Component {
 
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.state = {
-            chooseData: {},
-            playData: {
-                songs: [{
-                    id: "songN1",
-                    songName: "someSong",
-                    artist: "someArtist",
-                    album: "someAlbun",
-                    length: 435,
-                    filePath: '../songs/dfasdf'
-                  },
-                   {
-                    id: "songN2",
-                    songName: "someSong",
-                    artist: "someArtist",
-                    album: "someAlbun",
-                    length: 435,
-                    filePath: '../songs/dfasdf'
-                  },
-                   {
-                    id: "songN3",
-                    songName: "someSong",
-                    artist: "someArtist",
-                    album: "someAlbun",
-                    length: 435,
-                    filePath: '../songs/dfasdf'
-                  }]}
-        }
-        
-        this.addChooseData = this.addChooseData.bind(this);
-        this.addPlayData = this.addPlayData.bind(this);
-        this.startAnimation = this.startAnimation.bind(this);
-        this.startAnimationReverse = this.startAnimationReverse.bind(this);
-    }
+		this.state = {
+			showCar: false,
+			chooseData: {},
+			playData: {
+				songs: [{
+					id: "songN1",
+					songName: "someSong",
+					artist: "someArtist",
+					album: "someAlbun",
+					length: 435,
+					filePath: '../songs/dfasdf'
+				},
+				{
+					id: "songN2",
+					songName: "someSong",
+					artist: "someArtist",
+					album: "someAlbun",
+					length: 435,
+					filePath: '../songs/dfasdf'
+				},
+				{
+					id: "songN3",
+					songName: "someSong",
+					artist: "someArtist",
+					album: "someAlbun",
+					length: 435,
+					filePath: '../songs/dfasdf'
+				}]
+			}
+		}
 
-    addChooseData(data) {
-        this.setState({chooseData: data}, () => console.log('added new data to choose ', this.state.chooseData));
-    }
+		this.addChooseData = this.addChooseData.bind(this);
+		this.addPlayData = this.addPlayData.bind(this);
+		this.startAnimation = this.startAnimation.bind(this);
+		this.startAnimationReverse = this.startAnimationReverse.bind(this);
+	}
 
-    addPlayData(data) {
-        this.setState({playData: data}, () => console.log('added new data to playData ', this.state.playData));
-    }
+	addChooseData(data) {
+		this.setState({ chooseData: data }, () => console.log('added new data to choose ', this.state.chooseData));
+	}
 
-    
-  startAnimation() {
-    const car = document.querySelector('.car')
-    console.log(car);
-    car.className = "car run";
-  }
+	addPlayData(data) {
+		this.setState({ playData: data }, () => console.log('added new data to playData ', this.state.playData));
+	}
 
-  startAnimationReverse() {
-    const car = document.querySelector('.car')
-    car.className = "car run-reverse";
-  }
 
-    render() {
-        return(
-        <Router>
-            <React.Fragment>
-                <DevelopLinks />
-                <Header funcs={{start: this.startAnimation, reverse: this.startAnimationReverse}} />
-                <Switch>
-                    <Route exact path="/" component={App}/>
-                    <AppliedRoute path="/login" Component={Login} routeData={this.state.chooseData} funcs={{add: this.addChooseData}} />
-                    <AppliedRoute path="/home" Component={Home} routeData={this.state.chooseData} funcs={{add: this.addChooseData}} />
-                    <AppliedRoute path="/choose" Component={ChooseSongs} routeData={this.state.chooseData} funcs={{add: this.addChooseData}} />
-                    <AppliedRoute path="/play" Component={PlayList} routeData={this.state.playData} funcs={{add: this.addPlayData}} />
-                    <Route component={NotFound} />
-                </Switch>
-            </React.Fragment>
-        </Router>
-        );
-    }
+	startAnimation() {
+		const car = document.querySelector('.car')
+		console.log(car);
+		car.className = "car run";
+	}
+
+	startAnimationReverse() {
+		const car = document.querySelector('.car')
+		car.className = "car run-reverse";
+	}
+
+	render() {
+		const car = this.state.showCar ? <Car funcs={{ start: this.startAnimation, reverse: this.startAnimationReverse }} /> : null;
+
+		return (
+			<Router>
+				<React.Fragment>
+					<Header />
+					{car}
+					<Switch>
+						<Route exact path="/" component={App} />
+						<AppliedRoute path="/home" Component={Home} routeData={this.state.chooseData} funcs={{ add: this.addChooseData }} />
+						<AppliedRoute path="/choose" Component={ChooseSongs} routeData={this.state.chooseData} funcs={{ add: this.addChooseData }} />
+						<AppliedRoute path="/play" Component={PlayList} routeData={this.state.playData} funcs={{ add: this.addPlayData }} />
+						<Route component={NotFound} />
+					</Switch>
+				</React.Fragment>
+			</Router>
+		);
+	}
 }
 
 
