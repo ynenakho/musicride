a25238d823d3eb948b8ce1ad6386791a886f1543
terexion <sansyzbayev.alibek@gmail.com>
2019-03-02 17:37:25 -0800

    added react-bootstrap

---
+++ src/components/Layouts/MainPage/MaingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './MainPage.css';
+
+const layout = (props) => {
+    return (
+        <div className="styles.MaingPage">
+            
+        </div>
+    );
+};
+
+export default layout;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
+class Login extends Component {
+    email = "";
+    password = "";
+
+    validateForm() {
+        return (this.email.length > 0 && this.password.length > 6);
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value;
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="Login">
+              <form onSubmit={this.handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                  <ControlLabel>Email</ControlLabel>
+                  <FormControl
+                    autoFocus
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    type="password"
+                  />
+                </FormGroup>
+                <Button
+                  block
+                  bsSize="large"
+                  disabled={!this.validateForm()}
+                  type="submit"
+                >
+                  Login
+                </Button>
+              </form>
+            </div>
+          );
+    };
+}
+
+export default Login;
