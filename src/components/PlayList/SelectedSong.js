ddb3ef2ea5995ca3cfca25a2c1729868eb6567e4
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2019-03-02 19:59:46 -0800

    working on playing music

--- src/components/PlayList/SelectedSong.js
+++ src/components/PlayList/SelectedSong.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import Song from './Song';
+import Sound from 'react-sound';
 
-const SelectaedSong = ({ song }) => {
-  if (!song) {
+class SelectaedSong extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      controlled: true,
+      currentSong: this.props.song,
+      position: 0,
+      volume: 100,
+      playbackRate: 1,
+      loop: false,
+      playStatus: Sound.status.PLAYING
+    };
+  }
+  render() {
+  if (!this.props.song) {
     return <div>Song not selected</div>;
   }
 
   // const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
-  const {id, songName, artist, album, length, filePath} = song;
+  const {id, songName, artist, album, length, filePath} = this.props.song;
+  const { volume, playbackRate, loop } = this.state;
   return (
     <div>
+      {/* <Sound
+        url={"../../.." + filePath}
+        playStatus={Sound.status.PLAYING}
+        playFromPosition={0 }
+      />  */}
+      <Sound
+              url='https://raw.githubusercontent.com/scottschiller/SoundManager2/master/demo/_mp3/1hz-10khz-sweep.mp3'
+              playStatus={this.state.playStatus}
+              position={this.state.position}
+              volume={volume}
+              playbackRate={playbackRate}
+              loop={loop}
+              onLoading={({ bytesLoaded, bytesTotal }) => console.log(`${bytesLoaded / bytesTotal * 100}% loaded`)}
+              onLoad={() => console.log('Loaded')}
+              onPlaying={({ position }) => this.setState({ position })}
+              onPause={() => console.log('Paused')}
+              onResume={() => console.log('Resumed')}
+              onStop={() => console.log('Stopped')}
+              onFinishedPlaying={() => this.setState({ playStatus: Sound.status.STOPPED })}
+            />
       <Song 
         songName={songName}
         id={id}
@@ -28,6 +64,7 @@ const SelectaedSong = ({ song }) => {
       </div> */}
     </div>
   );
+}
 }
 
 export default SelectaedSong;
--- src/routes.js
+++ src/routes.js
@@ -36,11 +36,11 @@ class Routes extends React.Component {
             playData: {
                 songs: [{
                     id: "songN1",
-                    songName: "someSong1",
-                    artist: "someArtist1",
+                    songName: "One Kiss",
+                    artist: "Dua Lipa",
                     album: "someAlbun1",
                     length: 4351,
-                    filePath: '../songs/dfasdf1'
+                    filePath: '/public/assets/songs/Duke Dumont - Ocean Drive.mp3'
                   },
                    {
                     id: "songN2",
@@ -48,7 +48,7 @@ class Routes extends React.Component {
                     artist: "someArtist2",
                     album: "someAlbun2",
                     length: 4352,
-                    filePath: '../songs/dfasdf2'
+                    filePath: '/public/assets/songs/Pharrell Williams - Happy.mp3'
                   },
                    {
                     id: "songN3",
@@ -56,7 +56,7 @@ class Routes extends React.Component {
                     artist: "someArtist3",
                     album: "someAlbun3",
                     length: 4353,
-                    filePath: '../songs/dfasdf3'
+                    filePath: '/public/assets/songs/Queen - We Are the Champions.mp3'
                   }]}
         }
         
