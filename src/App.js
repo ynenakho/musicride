0ed8778b616a610ee512baf8123205dfb40800a4
terexion <sansyzbayev.alibek@gmail.com>
2019-03-03 02:15:06 -0800

    added home

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
 import React from 'react';
 // import Header from './components/Header';
 import PlaySongs from './components/PlayList/PlayList';
-import { Jumbotron, Button, Container } from 'react-bootstrap';
+import { Form, Button, Col, Container, Row } from 'react-bootstrap';
 
 
 class App extends React.Component {
-  song1 = {
-    id: "songN1",
-    songName: "someSong",
-    artist: "someArtist",
-    album: "someAlbun",
-    length: 435,
-    filePath: '../songs/dfasdf'
-  };
-  song2 = {
-    id: "songN2",
-    songName: "someSong",
-    artist: "someArtist",
-    album: "someAlbun",
-    length: 435,
-    filePath: '../songs/dfasdf'
-  };
-  song3 = {
-    id: "songN3",
-    songName: "someSong",
-    artist: "someArtist",
-    album: "someAlbun",
-    length: 435,
-    filePath: '../songs/dfasdf'
-  };
-  songs = [this.song1, this.song2, this.song3];
+  state = {
+    whereFrom: this.props.whereFrom,
+    whereTo: this.props.whereTo
+  }
 
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+    this.props.confirmRide({ whereFrom: event.target.elements.whereFrom.value, whereTo: event.target.elements.whereTo.value });
+    this.props.history.push('/choose');
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
     return (
-      <Container className="ui container">
-        <Jumbotron>
-          <h1>Hello, world!</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-         </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
+      <Container>
+        <Row noGutters>
+          <Form xs={6} onSubmit={this.onSubmitHandler}>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Where from:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="whereFrom"
+                  value={this.state.whereFrom}
+                  onChange={this.onChangeHandler}
+                  disabled={this.props.rideSubmitted}
+                />
+              </Form.Group>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Where to:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={this.state.whereTo}
+                  onChange={this.onChangeHandler}
+                  name="whereTo"
+                  disabled={this.props.rideSubmitted}
+                />
+              </Form.Group>
+            </Form.Row>
+            <Button variant="primary" type="submit">
+              Confirm
+						</Button>
+          </Form>
+
+        </Row>
       </Container>
-    )
-  }
-}
+    )}
+};
 
 export default App;
